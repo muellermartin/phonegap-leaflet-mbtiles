f15348ff2c25c768b3af70e91fce93ef74f6df18
Martin Müller <mail@mueller-martin.net>
2013-09-29 11:38:58 +0200

    Download MBTiles file and show tiles in current view

---
+++ www/js/TileLayer.MBTiles.js
@@ -0,0 +1,38 @@
+// inspired by: https://github.com/coomsie/topomap.co.nz/blob/master/Resources/leaflet/TileLayer.DB.js
+L.TileLayer.MBTiles = L.TileLayer.extend({
+	// SQLitePlugin database
+	mbTilesDB: null,
+
+	initialize: function(db, options) {
+		console.log('Constructor for L.TileLayer.MBTiles');
+		L.Util.setOptions(this, options);
+
+		this.mbTilesDB = db;
+	},
+
+	getTileUrl: function(tilePoint, tile) {
+		console.log('Requesting tile URL at X: ' + tilePoint.x + ' Y: ' + tilePoint.y + ' Z: ' + tilePoint.z);
+		var z = tilePoint.z;
+		var x = tilePoint.x;
+		var y = tilePoint.y;
+
+		this.mbTilesDB.transaction(function(tx) {
+			tx.executeSql('SELECT tile_data FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?', [z, x, y], function (tx, result) {
+				console.log('SQL query executed successfully!');
+				tile.src = 'data:image/png;base64,' + result.rows.item(0).tile_data;
+				//tile.src = 'http://localhost/~martin/test.png';
+			}, function (tx, error){
+				console.log('ERROR: ' + error.message);
+			});
+		});
+	},
+
+	_loadTile: function(tile, tilePoint) {
+		tile._layer = this;
+		tile.onload = this._tileOnLoad;
+		tile.onerror = this._tileOnError;
+		//tile.src = 'http://localhost/~martin/test.png';
+		this._adjustTilePoint(tilePoint);
+		this.getTileUrl(tilePoint, tile);
+	}
+});
--- www/js/index.js
+++ www/js/index.js
@@ -29,21 +29,63 @@ var app = {
 
 		console.log('Received Event: ' + id);
 
+		if(!fs)
+			console.log('fs in uninitialized - that\'s good!');
+		else
+			console.log('shit: can\'t check if fs is initialized');
+
 		// Request filesystem
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, app.onFileSystemSuccess, app.onFileSystemError);
-
-		var map = L.map('map').setView([51.505, -0.09], 13);
 	},
 
 	onFileSystemSuccess: function(fileSystem) {
 		console.log('File system name: ' + fileSystem.name);
 		console.log('File system root name: ' + fileSystem.root.name);
 		fs = fileSystem;
+
+		app.prepareTiles();
 	},
 
 	onFileSystemError: function(event) {
 		console.log('Failed to initialze file system');
+	},
+
+	prepareTiles: function() {
+		if(fs)
+		{
+			var tiles = fs.root.getFile('SpatialiteTest.sqlite', { create: false }, function() {
+				console.log('file exists');
+				app.loadTiles();
+			}, function() {
+				console.log('file does not exist');
+				console.log('downloading file');
+
+				var ft = new FileTransfer();
+
+				ft.download('http://localhost/~martin/SpatialiteTest.mbtiles', fs.root.fullPath + '/SpatialiteTest.sqlite', function(entry) {
+					console.log('download complete: ' + entry.fullPath);
+					app.loadTiles();
+				}, function(error) {
+					console.log('download error: ' + error);
+				});
+			});
+		}
+	},
+	//*
+	loadTiles: function() {
+		console.log('Executing loadTiles');
+		var db = window.sqlitePlugin.openDatabase({ name: "SpatialiteTest.sqlite", bgType: 1 });
+		var map = L.map('map', {
+			center: new L.LatLng(49.5964, 11.0077),
+			zoom: 15,
+			minZoom: 15,
+			maxZoom: 17
+		});
+		var layer = new L.TileLayer.MBTiles(db, { tms: true });
+
+		map.addLayer(layer);
 	}
+	//*/
 };
 
 app.initialize();
